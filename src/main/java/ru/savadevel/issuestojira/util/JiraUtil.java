ddb0d580ea3431007d20ed10efb6ab540e7f66ba
Savadevel <savadevel@gmail.com>
2022-07-10 21:55:15 +0300

    Добавлено формирование отчета по результатам загрузки задач в Jira

--- src/main/java/ru/savadevel/issuestojira/util/JiraUtil.java
+++ src/main/java/ru/savadevel/issuestojira/util/JiraUtil.java
@@ -1,5 +1,6 @@
 package ru.savadevel.issuestojira.util;
 
+import ru.savadevel.issuestojira.model.IssueJira;
 import ru.savadevel.issuestojira.model.ProjectJira;
 import ru.savadevel.issuestojira.service.JiraService;
 import ru.savadevel.issuestojira.to.FormTo;
@@ -10,19 +11,24 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JiraUtil {
     private JiraUtil() {
     }
 
-    public static void createIssue(FormTo formTo) {
+    public static Map<String, IssueJira> createIssue(FormTo formTo) {
         ProjectJira projectJira = getProjectJira(formTo);
+        Map<String, IssueJira> result = new HashMap<>();
         try (JiraService jiraService = new JiraService(formTo.getUrl(), formTo.getUsername(), formTo.getPassword())) {
-            projectJira.getIssues()
-                    .forEach(jiraService::createIssue);
+            for (IssueJira issue : projectJira.getIssues()) {
+                result.put(jiraService.createIssue(issue), issue);
+            }
         } catch (Exception e) {
             throw new ApplicationException(e.getMessage());
         }
+        return result;
     }
 
     private static ProjectJira getProjectJira(FormTo formTo) {
--- src/main/java/ru/savadevel/issuestojira/web/JiraController.java
+++ src/main/java/ru/savadevel/issuestojira/web/JiraController.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import ru.savadevel.issuestojira.model.IssueJira;
 import ru.savadevel.issuestojira.to.FormTo;
 import ru.savadevel.issuestojira.util.JiraUtil;
 import ru.savadevel.issuestojira.util.exception.IllegalRequestDataException;
 import ru.savadevel.issuestojira.util.validation.FormToValidation;
 
 import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class JiraController {
@@ -19,7 +21,9 @@ public class JiraController {
     private static final Logger log = LoggerFactory.getLogger(JiraController.class);
 
     private static final String FORM_LOAD_ISSUES = "form";
+    private static final String FORM_ATTRIBUTE = "formTo";
     private static final String REPORT_LOAD_ISSUES = "report";
+    private static final String REPORT_ATTRIBUTE = "report";
 
     private final FormToValidation formToValidation;
 
@@ -30,17 +34,18 @@ public class JiraController {
     @GetMapping("/")
     public String form(Model model) {
         log.info("Request form load issues to Jira");
-        model.addAttribute("formTo", new FormTo());
+        model.addAttribute(FORM_ATTRIBUTE, new FormTo());
         return FORM_LOAD_ISSUES;
     }
 
     @PostMapping("/")
-    public String submit(@Valid @ModelAttribute("formTo") FormTo formTo, BindingResult bindingResult) {
+    public String submit(@Valid @ModelAttribute(FORM_ATTRIBUTE) FormTo formTo, BindingResult bindingResult, Model model) {
         log.info("Submit form load issues to Jira: {} {}", formTo.getUrl(), formTo.getUsername());
         formToValidation.validate(formTo, bindingResult);
         if (bindingResult.hasErrors())
             throw new IllegalRequestDataException(bindingResult);
-        JiraUtil.createIssue(formTo);
+        Map<String, IssueJira> report = JiraUtil.createIssue(formTo);
+        model.addAttribute(REPORT_ATTRIBUTE, report);
         return REPORT_LOAD_ISSUES;
     }
 }
