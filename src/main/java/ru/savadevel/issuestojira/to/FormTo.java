b4acc33e4e1a1efca8f1a3431edaa389569130d0
Savadevel <savadevel@gmail.com>
2022-07-21 22:17:39 +0300

    Явное задание импортов

--- src/main/java/ru/savadevel/issuestojira/to/FormTo.java
+++ src/main/java/ru/savadevel/issuestojira/to/FormTo.java
@@ -1,6 +1,9 @@
 package ru.savadevel.issuestojira.to;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
